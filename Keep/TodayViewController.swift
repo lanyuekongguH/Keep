e7e20716094bf863e3664bc5bf6ae183c4c758f0
lanyuekongguH <lanyuekonggu@163.com>
2017-03-29 18:01:14 +0800

    Today Extension

---
+++ Keep/TodayViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TodayViewController.swift
+//  KEEP
+//
+//  Created by Damon on 17/3/29.
+//  Copyright © 2017年 Damon. All rights reserved.
+//
+
+import UIKit
+import NotificationCenter
+
+class TodayViewController: UIViewController, NCWidgetProviding {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        // Perform any setup necessary in order to update the view.
+        
+        // If an error is encountered, use NCUpdateResult.Failed
+        // If there's no update required, use NCUpdateResult.NoData
+        // If there's an update, use NCUpdateResult.NewData
+        
+        completionHandler(NCUpdateResult.newData)
+    }
+    
+}
