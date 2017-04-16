e674ed93d644ace690d061f7ced334314de46441
lanyuekongguH <Damon@shumingmingdeMacBook-Pro.local>
2017-04-16 13:38:58 +0800

    Removed UserInterfaceState.xcuserstate

--- Keep/Classes/News/View/KPNewsAddFriendListCell.swift
+++ Keep/Classes/News/View/KPNewsAddFriendListCell.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class KPNewsAddFriendListCell: UITableViewCell {
 
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?){
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupUI()
+    }
     
+    fileprivate func setupUI() {
+        
+    }
     
     required init?(coder aDecoder: NSCoder){
         fatalError("init(coder:) has not been implemented")
--- Keep/Classes/News/View/KPNewsHotHeadView.swift
+++ Keep/Classes/News/View/KPNewsHotHeadView.swift
@@ -16,6 +16,9 @@ class KPNewsHotHeadView: UIView {
         
     }
     
+    
+    
+    
     required init?(coder aDecoder: NSCoder){
         fatalError("init(coder:) has not been implemented")
     }
