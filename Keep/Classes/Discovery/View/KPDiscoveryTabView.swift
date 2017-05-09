ab6fc92fd0c3fca6b4a712e858f2133b28fec870
lanyuekongguH <lanyuekonggu@163.com>
2017-05-09 14:41:30 +0800

    精选

--- Keep/Classes/Discovery/View/KPDiscoveryTabView.swift
+++ Keep/Classes/Discovery/View/KPDiscoveryTabView.swift
@@ -25,15 +25,20 @@ class KPDiscoveryTabView: UIView {
             
             if let tabTitles = tabTitles {
                 
+                let buttonW :CGFloat = 50.0
+                let buttonD = (SCREENW - CGFloat(50 * tabTitles.count))/CGFloat(tabTitles.count * 2)
+
                 for i in 0..<tabTitles.count {
                     
+                    let buttonX = buttonD * CGFloat(i*2+1) + buttonW * CGFloat(i)
                     let button = UIButton()
+                    button.frame = CGRect.init(x: buttonX, y: 0, width: buttonW, height: 44)
                     button.tag = i
                     button.addTarget(self, action: #selector(buttonClick(_:)), for: .touchUpInside)
                     button.setTitle(tabTitles[i], for: UIControlState())
                     button.setTitleColor(KPGreen(), for: UIControlState())
-                    
-                    
+                    button.titleLabel?.font = UIFont.systemFont(ofSize: 16)
+                    addSubview(button)
                 }
                 
             }
