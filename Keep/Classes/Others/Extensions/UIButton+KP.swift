5328d8757b1c69b50a0d6744e73238dea257035e
lanyuekonggu <lanyuekonggu@163.com>
2017-06-09 23:31:55 +0800

    run share

---
+++ Keep/Classes/Others/Extensions/UIButton+KP.swift
@@ -0,0 +1,15 @@
+//
+//  UIButton+KP.swift
+//  Keep
+//
+//  Created by 舒明明 on 2017/6/9.
+//  Copyright © 2017年 Damon. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIButton {
+    
+    
+}
---
+++ Keep/Classes/Train/View/KPTrainShareTabView.swift
@@ -0,0 +1,42 @@
+//
+//  KPTrainShareTabView.swift
+//  Keep
+//
+//  Created by 舒明明 on 2017/6/9.
+//  Copyright © 2017年 Damon. All rights reserved.
+//
+
+import UIKit
+
+public enum UITableViewCellEditingStyle : Int {
+    
+    case street
+    
+    case path
+    
+    case map
+    
+    case consume
+    
+    case speed
+    
+    case data
+}
+
+protocol TrainShareTabDelegate: NSObjectProtocol {
+    func trainShareTabDelegate(_ view:KPTrainShareTabView, button:UIButton)
+}
+
+class KPTrainShareTabView: UIScrollView {
+
+    override init(frame:CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    fileprivate func setupUI() {
+        
+    }
+
+}
